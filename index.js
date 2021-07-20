a6a95fd08e6c5d9a2cb33f7ef241f811a7e87377
josevitorbraga <53883205+josevitorbraga@users.noreply.github.com>
2021-07-20 17:55:16 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const got = require('got')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+app.get("/", async(req, res)=>{
+    // TODO: error handler
+    const response = await got('https://sites.madnezz.com.br/api/site/json/loja.asp?tipo=1&shopping_id=112&llj=true&full=true&jsoncallback=jQuery33103885199709465923_1626204202237&_=1626204202238')
+    const responseJSON = JSON.parse(response.body.slice(41,-1))
+
+    res.json(responseJSON)
+})
+
+app.listen(3333, ()=>{
+    console.log("Server sarted")
+})
